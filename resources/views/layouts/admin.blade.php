a2c892f4bea108efcb8238a51a86dace47751848
pankaj sengar <pks11iitr@gmail.com>
2019-12-18 18:33:27 +0530

    error success view added

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -200,6 +200,47 @@
         </div>
         <!-- /.sidebar -->
     </aside>
+    <div>
+        @if ($message = Session::get('success'))
+            <div class="alert alert-success alert-block">
+                <button type="button" class="close" data-dismiss="alert">×</button>
+                <strong>{{ $message }}</strong>
+            </div>
+        @endif
+
+
+        @if ($message = Session::get('error'))
+            <div class="alert alert-danger alert-block">
+                <button type="button" class="close" data-dismiss="alert">×</button>
+                <strong>{{ $message }}</strong>
+            </div>
+        @endif
+
+
+        @if ($message = Session::get('warning'))
+            <div class="alert alert-warning alert-block">
+                <button type="button" class="close" data-dismiss="alert">×</button>
+                <strong>{{ $message }}</strong>
+            </div>
+        @endif
+
+
+        @if ($message = Session::get('info'))
+            <div class="alert alert-info alert-block">
+                <button type="button" class="close" data-dismiss="alert">×</button>
+                <strong>{{ $message }}</strong>
+            </div>
+        @endif
+
+    <!-- this is for validation errors -->
+        @if ($errors->any())
+            <?php var_dump($errors); ?>
+            <div class="alert alert-danger">
+                <button type="button" class="close" data-dismiss="alert">×</button>
+                Please check the form below for errors
+            </div>
+        @endif
+    </div>
 
     @yield('content')
     <footer class="main-footer">

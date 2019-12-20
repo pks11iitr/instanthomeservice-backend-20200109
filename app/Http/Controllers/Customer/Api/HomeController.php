ca8482cbbb03dea2caab7c9eff517410b58b597c
pankaj sengar <pks11iitr@gmail.com>
2019-12-20 18:26:16 +0530

    minor fixes

--- app/Http/Controllers/Customer/Api/HomeController.php
+++ app/Http/Controllers/Customer/Api/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends Controller
 {
     public function index(Request $request){
         $banner=Banner::active()->get();
-        $maincategories=Category::active()->get();
+        $maincategories=Category::active()->where('parent', 0)->get();
         //$smallcategories=Category::active()->where('istop', false)->limit(6)->get();
         $partners=Partners::active()->get();
         return compact('banner','maincategories', 'partners');

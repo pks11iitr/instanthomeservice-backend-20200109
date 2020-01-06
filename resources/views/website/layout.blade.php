70960b79ee56bf87a2b3a414e2a938738f9b275c
pankaj sengar <pks11iitr@gmail.com>
2020-01-06 18:23:15 +0530

    home url set

--- resources/views/website/layout.blade.php
+++ resources/views/website/layout.blade.php
@@ -56,7 +56,7 @@
                 <span class="icon-bar"></span>
             </a>
             <div class="navbar-inner">
-                <a class="brand" href="index.html"><img src="{{asset('vendor/themes/images/logo.png')}}" alt="Bootsshop"/></a>
+                <a class="brand" href="{{env('APP_URL')}}"><img src="{{asset('vendor/themes/images/logo.png')}}" alt="Bootsshop"/></a>
                 <form class="form-inline navbar-search" method="post" action="products.html" >
                     <input id="srchFld" class="srchTxt" type="text" />
                     <select class="srchTxt">

ac09f7cee3f3517f95f9e920166f27004c24c06e
pankaj sengar <pks11iitr@gmail.com>
2020-01-08 17:08:49 +0530

    error display on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,18 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
+                        <div class="form-group row">
+                            <div class="col-md-6">
+                                @if ($message = Session::get('error'))
+                                    <span style="color:red">{{$message}}</span>
+                                @endif
+                            </div>
+                            <div class="col-md-6">
+                                @if ($message = Session::get('success'))
+                                    <span style="color:green">{{$message}}</span>
+                                @endif
+                            </div>
+                        </div>
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Mobile') }}</label>
 

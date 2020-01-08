46f12c230282725eb60cecfc32df85a74e9d74d5
pankaj sengar <pks11iitr@gmail.com>
2020-01-08 17:02:35 +0530

    login change

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
 class LoginController extends Controller
 {
     /*
@@ -52,14 +54,24 @@ class LoginController extends Controller
     }
 
     public function redirectTo(){
-        foreach(config('allowedusers.admins') as $key=>$value){
-            if(auth()->user()->hasRole($key)){
-                return route($value);
+        if(auth()->user()->status==1) {
+            foreach (config('allowedusers.admins') as $key => $value) {
+                if (auth()->user()->hasRole($key)) {
+                    return route($value);
+                }
             }
+            Auth::logout();
+            Session::flash('error', 'You dont have permissions to login here');
+            return route('login');
+        }else if(auth()->user()->status==0){
+            Auth::logout();
+            Session::flash('error', 'Account is not active');
+            return route('login');
+        }else{
+            Auth::logout();
+            Session::flash('error', 'Account has been blocked');
+            return route('login');
         }
-
-        Auth::logout();
-        abort(401);
     }
 
 

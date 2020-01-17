ac5b35b705af4897690efa0eec93ba6e4ed94c13
pankaj sengar <pks11iitr@gmail.com>
2020-01-17 16:12:50 +0530

    remove password from login

--- app/Http/Controllers/Auth/Api/LoginController.php
+++ app/Http/Controllers/Auth/Api/LoginController.php
@@ -66,7 +66,7 @@ class LoginController extends Controller
     {
         return User::create([
             'mobile' => $data['mobile'],
-            'password' => Hash::make($data['password'])
+            //'password' => Hash::make($data['password'])
         ]);
     }
 

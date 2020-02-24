b0a130663700153b35a62b9761cfc1fcd91c60fd
ravi <ravisaini9900@gmail.com>
2020-02-24 13:50:59 +0530

    New coupon and payment

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -329,6 +329,29 @@
                             </li>
                         </ul>
                     </li>
+                    <li class="nav-item has-treeview">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-table"></i>
+                            <p>
+                                Coupons
+                                <i class="fas fa-angle-left right"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            {{--<li class="nav-item">
+                                <a href="{{route('coupons.create')}}" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Coupons Add</p>
+                                </a>
+                            </li>--}}
+                            <li class="nav-item">
+                                <a href="{{route('coupons.list')}}" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Coupons View</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
                 </ul>
             </nav>
             <!-- /.sidebar-menu -->

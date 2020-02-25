88b77ebb87d268456dbfe67cbfcb5688c5130b7e
pankaj sengar <pks11iitr@gmail.com>
2020-02-25 14:17:25 +0530

    sms api key changed

--- app/Services/SMS/Msg91.php
+++ app/Services/SMS/Msg91.php
@@ -7,14 +7,14 @@ namespace App\Services\SMS;
 class Msg91
 {
 
-    protected static $authkey='308501AiM8VCGp6w4I5df71314';
+    protected static $authkey='320062AbqIyo1F643V5e54de49P1';
 
     public static function send($mobile, $message){
         return true;
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://api.msg91.com/api/sendhttp.php?authkey=".self::$authkey."&mobiles=$mobile&unicode=&country=91&message=$message&sender=PTMNRA&route=4",
+            CURLOPT_URL => "https://api.msg91.com/api/sendhttp.php?authkey=".self::$authkey."&mobiles=$mobile&unicode=&country=91&message=$message&sender=ZAPAAK&route=4",
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
             CURLOPT_MAXREDIRS => 10,
